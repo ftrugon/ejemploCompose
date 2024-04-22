c429bc50dc74cb50fafdcb2d891d88eccb249693
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-04-22 21:35:56 +0200

    talll

---
+++ src/main/kotlin/Ej2.kt
@@ -0,0 +1,53 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+
+@Composable
+@Preview
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+
+        contentAlignment = Alignment.Center
+    ){
+        Box(
+            modifier = Modifier.background(Color.Cyan).width(300.dp).height(200.dp),
+            contentAlignment = Alignment.BottomCenter
+            ){
+                Text("Hola esto es un ejemplo del box")
+            }
+    }
+
+
+}
+
+fun main() = application {
+
+    val windowState = rememberWindowState(size = DpSize(1200.dp,800.dp))
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Achili pum",
+        state = windowState
+
+    ) {
+        MainScreen()
+    }
+}
